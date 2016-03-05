a234dc67993a4a075257ba8a129965c4dd7fdf28
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2016-03-05 14:57:28 +0900

    This version implements the method for indent

--- javascripts/BeautifulEditor.js
+++ javascripts/BeautifulEditor.js
@@ -123,6 +123,21 @@
         }
     };
 
+    /**
+     * This method adds event listener for indent.
+     * @param {EventTarget} eventTarget This argument is the instance of EventTarget.
+     * @param {string} eventType This argument is string for event type.
+     */
+    BeautifulEditor.prototype.indent = function(eventTarget, eventType) {
+        var self = this;
+
+        if (eventTarget instanceof EventTarget) {
+            eventTarget.addEventListener(String(eventType), function(event) {
+                self.contentDocument.execCommand('indent', false, null);
+            }, false);
+        }
+    };
+
     // Export
     global.BeautifulEditor = BeautifulEditor;
 
