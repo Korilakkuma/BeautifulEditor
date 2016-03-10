9777b919960f883e966bb209fb34dae84117e7cb
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2016-03-10 10:42:14 +0900

    This version implements the method for unordered list

--- javascripts/BeautifulEditor.js
+++ javascripts/BeautifulEditor.js
@@ -168,6 +168,21 @@
         }
     };
 
+    /**
+     * This method adds event listener for unordered list.
+     * @param {EventTarget} eventTarget This argument is the instance of EventTarget.
+     * @param {string} eventType This argument is string for event type.
+     */
+    BeautifulEditor.prototype.insertUnorderedList = function(eventTarget, eventType) {
+        var self = this;
+
+        if (eventTarget instanceof EventTarget) {
+            eventTarget.addEventListener(String(eventType), function(event) {
+                self.contentDocument.execCommand('insertUnorderedList', false, null);
+            }, false);
+        }
+    };
+
     // Export
     global.BeautifulEditor = BeautifulEditor;
 
