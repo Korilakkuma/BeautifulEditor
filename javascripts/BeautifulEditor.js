c2df5ba19f0a521d2d5be150586845ee81c05df6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2016-03-11 11:04:22 +0900

    This version implements the method for paragraph

--- javascripts/BeautifulEditor.js
+++ javascripts/BeautifulEditor.js
@@ -183,6 +183,21 @@
         }
     };
 
+    /**
+     * This method adds event listener for paragraph.
+     * @param {EventTarget} eventTarget This argument is the instance of EventTarget.
+     * @param {string} eventType This argument is string for event type.
+     */
+    BeautifulEditor.prototype.insertParagraph = function(eventTarget, eventType) {
+        var self = this;
+
+        if (eventTarget instanceof EventTarget) {
+            eventTarget.addEventListener(String(eventType), function(event) {
+                self.contentDocument.execCommand('insertParagraph', false, null);
+            }, false);
+        }
+    };
+
     // Export
     global.BeautifulEditor = BeautifulEditor;
 
